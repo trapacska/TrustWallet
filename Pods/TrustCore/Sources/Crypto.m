0ef22b81ffa04ca33619ca26812b57341316ae35
Tamas Papik <trapacska@gmail.com>
2019-09-13 16:13:59 +0200

    Update Crypto.m

--- Pods/TrustCore/Sources/Crypto.m
+++ Pods/TrustCore/Sources/Crypto.m
@@ -2,7 +2,7 @@
 //
 // This file is part of Trust. The full Trust copyright notice, including
 // terms governing use, modification, and redistribution, is contained in the
-// file LICENSE at the root of the source code distribution tree. +++
+// file LICENSE at the root of the source code distribution tree.
 
 #import "Crypto.h"
 #import <TrezorCrypto/TrezorCrypto.h>
